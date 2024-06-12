6b35f76eaf94fe78291666b6ba0a795dbcbec664
Evanlabanan <percevault@et.esiea.fr>
2024-06-12 19:27:19 +0200

    finish menu d'acceuil

--- SPS/Main.cpp
+++ SPS/Main.cpp
@@ -110,7 +110,7 @@ int main(int argc, char *argv[])
     playerConfig.SetTeamID(1);
     playerConfig.playerID = 1;
     playerConfig.skinID = 0;
-    playerConfig.isCPU = true;
+    playerConfig.isCPU = false;
     configs[1] = playerConfig;
 
     playerConfig.type = PlayerConfig::Type::FIRE_WARRIOR;
